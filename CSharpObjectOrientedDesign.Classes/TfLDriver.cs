7f85bd971ac7a9b683c1e3875d495f711a18f83f
Eason Shao <eason.syc@icloud.com>
2024-02-05 22:03:05 +0000

    add TfL Driver Class, inherited from the Driver class

---
+++ CSharpObjectOrientedDesign.Classes/TfLDriver.cs
@@ -0,0 +1,28 @@
+﻿namespace CSharpObjectOrientedDesign.Classes
+{
+	internal class TfLDriver : Driver
+	{
+		internal enum TfLType
+		{
+			Overground,
+			Underground,
+			Tram,
+			DLR,
+			ElizabethLine
+		}
+
+		internal TfLType Type { get; private set; }
+
+		internal TfLDriver(string name, string id, string type) : base(name, id)
+		{
+			if (Enum.TryParse(type, out TfLType convertedType)){
+				Type = convertedType;
+			}
+			else
+			{
+				throw new ArgumentException($"Type of driver invalid: {type}");
+			}
+		}
+	}
+}
+
